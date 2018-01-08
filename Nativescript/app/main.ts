a3e5b9a27effb0bb26725af64c3ad9826b525e50
ericnelson12 <ericjnelson012@gmail.com>
2018-01-07 20:41:40 -0800

    App automatically registers for the push notification topic news and the app can be send push notifications from any POST request

--- Nativescript/app/main.ts
+++ Nativescript/app/main.ts
@@ -1,4 +1,12 @@
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
+const firebase = require("nativescript-plugin-firebase");
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
+
+firebase.subscribeToTopic("news");
+
+firebase.getCurrentPushToken().then((token: string) => {
+    // may be null if not known yet
+    console.log("Current push token: " + token);
+  });
